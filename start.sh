f2fe93d4be396966bbacf9cccbca4d67b518004d
Alejandro Sanchez <emailforasr@gmail.com>
2018-06-19 00:11:39 -0700

    Initial commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Bring down any previously running containers
+docker-compose down
+
+# Build images
+echo 'Building images...'
+docker-compose build
+echo 'Complete'
+
+# Bring up new containers (silently)
+docker-compose up -d
